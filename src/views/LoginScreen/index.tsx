e0aff1610787aa5c34a071101372a0b9dd1bd630
Ibraim Barrios <barrios.party@hotmail.com>
2025-07-07 22:51:05 -0600

    add inputs and buttons to login

--- src/views/LoginScreen/index.tsx
+++ src/views/LoginScreen/index.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Button, Image, StyleSheet, TextInput, View } from 'react-native';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Image
-        style={styles.image}
-        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-      />
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        />
+      </View>
+      <View style={styles.inputsContainer}>
+        <TextInput placeholder="user" style={styles.textInput} />
+        <TextInput
+          placeholder="password"
+          style={styles.textInput}
+          secureTextEntry
+        />
+      </View>
+      <Button color="#1976D2" title="Sing in" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 18,
+    marginRight: 18,
+  },
   image: {
     width: 50,
     height: 50,
     alignSelf: 'center',
+  },
+  imageContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  inputsContainer: {
+    marginTop: 10,
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    padding: 6,
+    marginTop: 4,
+    marginBottom: 12,
+    fontSize: 16,
   },
 });
 
